ab6b8c58111b69af6b3bb0695be7597a3fbd98f3
Artur Moraes <artur.gomoraes@gmail.com>
2023-01-24 10:23:49 -0300

    fix: Change organization name

--- packages/docs/src/components/ColorsGrid.tsx
+++ packages/docs/src/components/ColorsGrid.tsx
@@ -1,4 +1,4 @@
-import { colors } from '@ignite-ui/tokens'
+import { colors } from '@dorumo-ui/tokens'
 import { getContrast } from 'polished'
 
 export function ColorsGrid() {
--- packages/docs/src/stories/MultiStep.stories.tsx
+++ packages/docs/src/stories/MultiStep.stories.tsx
@@ -1,4 +1,4 @@
-import { Box, MultiStep, MultiStepProps } from '@ignite-ui/react'
+import { Box, MultiStep, MultiStepProps } from '@dorumo-ui/react'
 import { Meta, StoryObj } from '@storybook/react'
 
 export default {
